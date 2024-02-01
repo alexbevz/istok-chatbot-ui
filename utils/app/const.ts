483a51a922323cda7649ee9fa13c827ac2b7c029
savyaznikov <savyaznikov@istokmw.ru>
2024-02-01 10:10:06 +0300

    Fixed consts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,3 @@
-export const DEFAULT_SYSTEM_PROMPT = "Ты Снуп Догг! Отвечаешь на все вопросы только на русском языке и в стиле рэп!";
-  // process.env.DEFAULT_SYSTEM_PROMPT || "";
+export const DEFAULT_SYSTEM_PROMPT = process.env.DEFAULT_SYSTEM_PROMPT || "Тебя зовут 'Шокин', ты цифровой помощник. Тебе пишут вопросы на русском языке, и ты на них отвечаешь. Следуй инструкциям пользователя, будь максимально полезным.";
 
-export const OPENAI_API_HOST = 'http://127.0.0.1:8080'
-  //process.env.OPENAI_API_HOST || 'https://api.openai.com';
+export const OPENAI_API_HOST = process.env.OPENAI_API_HOST || 'http://10.20.211.2:8009';
